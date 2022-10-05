4971cf7c6a086735e2fa2b6135696c41caf796b3
Josh Carlson <magneticstain@gmail.com>
2022-10-05 00:12:50 +0000

    Various updates

--- plexer.sh
+++ plexer.sh
@@ -32,12 +32,19 @@ fi
 normalize_directory() {
 	echo "Normalizing directory..."
         
-	BASE_DIR=$(echo "${MEDIA_FILE_DIR}" | rev | cut -d'/' -f2- | rev)
-	NEW_MEDIA_FILE_DIR="${BASE_DIR}/${BASE_NAME}"
-        
-#	mv "${MEDIA_FILE_DIR}" "${NEW_MEDIA_FILE_DIR}"
-        
+	BASE_DIR=$(echo "${MEDIA_FILE_DIR}" | rev | cut -d'/' -f3- | rev)
+	NEW_MEDIA_FILE_DIR="${BASE_DIR}/${BASE_NAME}/"
+
 	echo "[ ${MEDIA_FILE_DIR} => ${NEW_MEDIA_FILE_DIR} ]"
+
+        if [ "${MEDIA_FILE_DIR}" == "${NEW_MEDIA_FILE_DIR}" ]
+	then
+		echo "[INFO] current media directory and new media file directory are the same, not doing anything..."
+	else
+		mv "${MEDIA_FILE_DIR}" "${NEW_MEDIA_FILE_DIR}"
+        
+		MEDIA_FILE_DIR="${NEW_MEDIA_FILE_DIR}"
+	fi
 }
 
 normalize_files() {
@@ -52,12 +59,12 @@ normalize_files() {
 			NEW_MEDIA_FILE="${NEW_MEDIA_FILE_DIR}/${BASE_NAME}.${MEDIA_FILE_EXT}"
 
 			echo "Media file found [ ${MEDIA_FILE} ], renaming..."
-#        		mv "${MEDIA_FILE}" "${NEW_MEDIA_FILE}"
+			mv "${MEDIA_FILE}" "${NEW_MEDIA_FILE}"
         		echo "[ ${MEDIA_FILE} => ${NEW_MEDIA_FILE} ]"
 		else
 			# delete it
 			echo "Non-media file found [ ${MEDIA_FILE} ], deleting..."
-#			rm "${MEDIA_FILE}"
+			rm "${MEDIA_FILE}"
 		fi
 	done
 }
@@ -65,8 +72,14 @@ normalize_files() {
 move_media() {
 	echo "Moving to target media directory..."
         echo "[ ${NEW_MEDIA_FILE_DIR} => ${DST_MEDIA_DIR} ]"
-#	mv "${NEW_MEDIA_FILE_DIR}" "${DST_MEDIA_DIR}"
+	mv "${NEW_MEDIA_FILE_DIR}" "${DST_MEDIA_DIR}"
 }
+
+fix_media_file_perms() {
+        chown -R josh:media "${DST_MEDIA_DIR}"
+        chmod -R 775 "${DST_MEDIA_DIR}"
+}
+
 
 ### MAIN
 echo "Starting Plexer..."
@@ -76,6 +89,8 @@ echo ""
 
 for MEDIA_FILE_DIR in /mnt/volume_nyc1_01/media/Completed/*
 do
+	MEDIA_FILE_DIR="${MEDIA_FILE_DIR}/"
+
 	echo "############################################"
 	echo "CURRENT MEDIA DIR: ${MEDIA_FILE_DIR}"
 
@@ -87,5 +102,6 @@ do
 	normalize_files
 	echo ""
 	move_media
+	fix_media_file_perms
 	echo "############################################"
 done
