38be92176cf7c617ae28daff7e52a4840c768125
Josh Carlson <magneticstain@gmail.com>
2022-10-14 00:47:36 +0000

    Update PG URL in gutenberger to use HTTPS

--- gutenberger.sh
+++ gutenberger.sh
@@ -7,7 +7,7 @@
 read -r -p "PG BOOK ID #: " BOOK_ID
 read -r -p "BASE NAME: " BASE_NAME
 
-BOOK_URL="http://www.gutenberg.org/ebooks/${BOOK_ID}.kindle.images"
+BOOK_URL="https://www.gutenberg.org/ebooks/${BOOK_ID}.kindle.images"
 BOOK_FILE_NAME="/mnt/volume_nyc1_01/media/Books/auto-add/${BASE_NAME}.mobi"
 
 wget "${BOOK_URL}" -O "${BOOK_FILE_NAME}"
