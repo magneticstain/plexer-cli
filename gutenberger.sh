3f876f09cafb88e1aba46ef8c2635a88f874380e
Josh Carlson <magneticstain@gmail.com>
2022-10-14 00:46:17 +0000

    Add gutenberger script for easy book downloading

---
+++ gutenberger.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#
+# Simple script to easily download books from Project Gutenberg
+#
+
+read -r -p "PG BOOK ID #: " BOOK_ID
+read -r -p "BASE NAME: " BASE_NAME
+
+BOOK_URL="http://www.gutenberg.org/ebooks/${BOOK_ID}.kindle.images"
+BOOK_FILE_NAME="/mnt/volume_nyc1_01/media/Books/auto-add/${BASE_NAME}.mobi"
+
+wget "${BOOK_URL}" -O "${BOOK_FILE_NAME}"
