4648ff8a0c54a24bd3fc9031e2977b09f5da60af
Josh Carlson <837837+magneticstain@users.noreply.github.com>
2026-02-15 11:42:19 -0500

    Feat/40/integrate-metadata-generation (#86)
    * feat: integrate metadata generation and enhance file management
    
    - Add cli.sh for Codacy CLI v2 setup and version management
    - Create codacy.yaml for runtime and tool configurations
    - Update pyproject.toml to include prompt-toolkit dependency
    - Refactor file_manager.py to rename artifacts based on metadata
    - Implement heuristic analysis in metadata.py for extracting metadata from filenames
    - Modify main.py to improve logging functionality
    - Update uv.lock to include prompt-toolkit and its dependencies
    
    * feat: add dry run option for artifact processing
    
    * chore(tools): update trivy version used by codacy
    
    * feat(metadata): enhance metadata import and scrub artifact name functionality
    
    * feat(prompt): add user prompt behavior option for artifact processing
    
    `default` = Prompt if incomplete match
    `all` = Prompt for every artifact, regardless of heuristics match
    `none` = Never prompt (mainly used for automation)
    
    Closes: #40 , #32

--- src/plexer_cli/file_manager.py
+++ src/plexer_cli/file_manager.py
@@ -5,6 +5,8 @@ Module: File Manager - code for file-related ops
 """
 
 import os
+import re
+
 from pathlib import Path
 from magic import from_file
 from logzero import logger
@@ -12,6 +14,9 @@ from logzero import logger
 from .artifact import Artifact
 from .const import METADATA_FILE_NAME
 from .metadata import Metadata
+
+
+ARTIFACT_NAME_REGEX = r"^[ -~]+\([\d]{4}\) ?(\{[ -~]+\})?$"
 
 
 class FileManager:
@@ -28,8 +33,7 @@ class FileManager:
 
     def get_artifacts(self, tgt_dir="") -> list:
         """
-        Gather the names of all files and directories in a given directory and return as list
-
+        Gather the names of all files and directories in a given directory and return as list.
 
         Target directory is the source directory by default, but can be specified via parameter.
         """
@@ -72,17 +76,84 @@ class FileManager:
 
         return artifacts
 
-    def process_directory(self, dir_artifacts: list) -> None:
+    def check_artifact(self, artifact: Artifact) -> bool:
         """
-        Traverse the given directory artifacts, rename the
-          video files accordingly, and delete everything else
+        Perform any checks needed to determine if the artifact is valid for further processing
 
-        NOTE: the artifact for the metadata file MUST come first
-          in the artifact list. Failing to do so may lead to instability
-          during artifact processing.
+        Right now, this includes:
+            * Checking if the artifact name is in a valid format required by Plex
         """
 
-        video_metadata = Metadata()
+        valid_artifact = False
+
+        artifact_name_reg = re.compile(ARTIFACT_NAME_REGEX)
+        if artifact_name_reg.match(artifact.name):
+            logger.debug(
+                "artifact name is in a valid format for Plex: %s", artifact.name
+            )
+
+            valid_artifact = True
+        else:
+            logger.debug(
+                "artifact name is NOT in a valid format for Plex: %s", artifact.name
+            )
+
+        return valid_artifact
+
+    def rename_artifact(
+        self, artifact: Artifact, video_metadata: Metadata, dry_run=False
+    ) -> Artifact:
+        """
+        Rename an artifact to the new name generated from the given metadata
+
+        Returns the new, updated artifact object
+        """
+
+        new_artifact_name = f"{video_metadata.name} ({video_metadata.release_year})"
+
+        # get artifact file info for srrc/dst path generation
+        artifact_file_path = Path(artifact.absolute_path)
+        artifact_parent_dir = artifact_file_path.parent
+        artifact_file_ext = (
+            "" if artifact.mime_type == "directory" else artifact_file_path.suffix
+        )
+
+        src_file = artifact_file_path.absolute()
+        dst_file = f"{artifact_parent_dir}/{new_artifact_name}{artifact_file_ext}"
+
+        logger.debug(
+            "renaming artifact: [ OLD PATH: %s ] to [ NEW PATH: %s ]",
+            src_file,
+            dst_file,
+        )
+
+        if src_file != dst_file:
+            logger.debug(
+                "executing rename operation on filesystem: %s -> %s", src_file, dst_file
+            )
+            if not dry_run:
+                os.rename(src_file, dst_file)
+                artifact.name = new_artifact_name
+                artifact.absolute_path = dst_file
+            else:
+                logger.debug("dry run enabled; skipping actual rename operation")
+        else:
+            logger.debug(
+                "source and destination paths are identical; skipping rename operation"
+            )
+
+        return artifact
+
+    def process_directory(
+        self,
+        dir_artifacts: list,
+        video_metadata=Metadata(),
+        prompt_behavior="default",
+        dry_run=False,
+    ) -> None:
+        """
+        Traverse the given directory artifacts, rename the video files accordingly, and delete everything else
+        """
 
         logger.debug("starting directory artifact processing")
 
@@ -97,35 +168,53 @@ class FileManager:
             if artifact.mime_type == "directory":
                 logger.info("subdirectory found, processing")
 
-                # start recursive subprocessing
-                new_dir_artifacts = self.get_artifacts(tgt_dir=artifact.absolute_path)
-                self.process_directory(dir_artifacts=new_dir_artifacts)
-            elif artifact.name == METADATA_FILE_NAME:
-                # read in video metadata
-                logger.info("metadata file found, importing")
+                # first, check if we even need to do anything at all
+                if self.check_artifact(artifact=artifact):
+                    logger.info(
+                        "directory artifact is already in a valid format for Plex; skipping subprocessing"
+                    )
 
-                video_metadata.import_metadata_from_file(
-                    file_path=artifact.absolute_path
-                )
-            elif artifact.mime_type.startswith("video/"):
-                # move + rename
-                logger.info("video file found, renaming")
+                    continue
 
-                file_path = Path(artifact.absolute_path)
-                file_dir = file_path.parent
-                file_ext = file_path.suffix
+                # use heuristics to attempt to determine metadata from directory name
+                video_metadata_found = False
+                if video_metadata.do_heuristic_analysis(file_name=artifact.name):
+                    logger.info(
+                        "metadata found for directory via heuristics - name: %s, release_year: %d",
+                        video_metadata.name,
+                        video_metadata.release_year,
+                    )
+                    video_metadata_found = True
 
-                src_file = artifact.absolute_path
-                dst_file = (
-                    f"{file_dir}/{video_metadata.name}"
-                    f" ({video_metadata.release_year}){file_ext}"
-                )
+                if prompt_behavior == "all" or (
+                    prompt_behavior == "default" and not video_metadata_found
+                ):
+                    logger.info(
+                        "no metadata found for directory via heuristics; prompting user for manual input"
+                    )
+                    video_metadata.prompt_user_for_metadata()
 
-                logger.debug("moving %s to %s", src_file, dst_file)
+                if video_metadata.metadata_found:
+                    logger.info("renaming artifact based on gathered metadata")
+                    artifact = self.rename_artifact(
+                        artifact=artifact,
+                        video_metadata=video_metadata,
+                        dry_run=dry_run,
+                    )
 
-                os.rename(src_file, dst_file)
+                    # start recursive subprocessing
+                    new_dir_artifacts = self.get_artifacts(
+                        tgt_dir=artifact.absolute_path
+                    )
+                    if new_dir_artifacts:
+                        self.process_directory(
+                            dir_artifacts=new_dir_artifacts,
+                            video_metadata=video_metadata,
+                            dry_run=dry_run,
+                        )
+                else:
+                    logger.warning(
+                        "no metadata found for directory after exhausting all methods; skipping renaming and subprocessing"
+                    )
             else:
-                # delete the file
-                logger.info("unnecessary file found, deleting")
-
-                os.remove(artifact.absolute_path)
+                logger.info("file artifact found, processing")
--- src/plexer_cli/metadata.py
+++ src/plexer_cli/metadata.py
@@ -5,7 +5,9 @@ Module: Metadata - code for analyzing and managing video metadata
 """
 
 import json
+import re
 from logzero import logger
+from prompt_toolkit import PromptSession
 
 
 class Metadata:
@@ -14,11 +16,17 @@ class Metadata:
     """
 
     name = ""
-    release_year = 0
+    release_year = 1900
+    metadata_found = False
+    heuristics_patterns = {
+        "name": r"^(.+?)([\_\(\[])",  # anything before the first instance of commonly-used separators
+        "release_year": r"(19|20)([0-9]{2})",  # any 4 digit number between 1900 and 2099
+    }
 
     def __init__(self, name="", release_year=1900) -> None:
         self.name = name
-        self.release_year = release_year
+        if release_year >= 0:
+            self.release_year = release_year
 
     def import_metadata_from_file(self, file_path: str) -> None:
         """
@@ -39,3 +47,81 @@ class Metadata:
             logger.error(
                 'data missing in metadata file; "%s" field was not found', e.args[0]
             )
+
+    def scrub_artifact_name(self, artifact_name: str) -> str:
+        """
+        Remove common separators and file extensions from given artifact name.
+
+        Typically used to produce a cleaner string as part of heuristic analysis.
+        """
+
+        logger.debug("scrubbing artifact name: %s", artifact_name)
+
+        # replace common separators with spaces and strip surrounding whitespace
+        scrubbed_name = re.sub(r"[\.\_\-\(\)\[\]]+", " ", artifact_name).strip()
+
+        logger.debug("artifact name post-scrubbing: %s", scrubbed_name)
+
+        return scrubbed_name
+
+    def do_heuristic_analysis(self, file_name: str) -> bool:
+        """
+        Analyze given file name and attempt to extract metadata values via heuristics
+        """
+
+        logger.debug("performing heuristic analysis on artifact: %s", file_name)
+
+        # perform basic heuristics
+        ## NAME
+        possible_name = re.search(self.heuristics_patterns["name"], file_name)
+        if possible_name:
+            self.name = self.scrub_artifact_name(possible_name.group(1))
+        ## RELEASE YEAR
+        possible_release_year = re.findall(
+            self.heuristics_patterns["release_year"], file_name
+        )
+        if possible_release_year:
+            # if multiple years are found, take the last one since it's more likely to be the correct value
+            # (e.g. "Movie.Name.1999.1080p.2020" should yield 2020 as the release year, not 1999)
+            self.release_year = int(
+                "".join(possible_release_year[-1])
+            )  # Ex: [('19', '99'), ('20', '20')] -> ('20', '20') -> 2020
+
+        if possible_name and possible_release_year:
+            logger.debug(
+                "heuristic analysis results - name: %s, release_year: %d",
+                self.name,
+                self.release_year,
+            )
+
+            self.metadata_found = True
+            return True
+
+        logger.debug("heuristic analysis produced partial or no results")
+        self.metadata_found = (
+            False  # in case it has been overwritten from the default value
+        )
+        return False
+
+    def prompt_user_for_metadata(self) -> None:
+        """
+        Prompt the user for metadata values via CLI input
+        """
+
+        logger.debug("prompting user for metadata input")
+
+        prompt_sess = PromptSession()
+
+        user_name = prompt_sess.prompt(
+            "Enter the correct name for this media: ", default=self.name
+        )
+        user_release_year = prompt_sess.prompt(
+            "Enter the release year for this media: ",
+            default=str(
+                self.release_year
+            ),  # `default` argument _must_ be a string, per docs
+        )
+
+        self.name = user_name
+        self.release_year = int(user_release_year)
+        self.metadata_found = True
